69b389e11d591da6f05cd582bdb60c559d7aab4d
Richard Neese <r.neese@gmail.com>
2013-12-27 14:27:03 +0000

    fixeda install issue

--- install/Debian/Debian-Wheezy-PBX-PKG-Install.sh
+++ install/Debian/Debian-Wheezy-PBX-PKG-Install.sh
@@ -157,7 +157,7 @@ freeswitch_install="fusionpbx"
 # Suggests: <freeswitch-mod-spidermonkey>
 # Recommends: freeswitch-init freeswitch-lang freeswitch-music freeswitch-sounds freeswitch-conf-vanilla
 
-freeswitch_sounds="y"
+freeswitch_sounds="n"
 # Installs the freeswitch sounds en-us-callie
 
 freeswitch_music="y"
@@ -434,16 +434,16 @@ if [[ $freeswitch_install == "fusionpbx" ]]; then
 	#Pkgs needed for faxing (freeswitch-mod-spandsp)
 	for i in libtiff5 libtiff-tools ghostscript ;do apt-get -y install "${i}" ; done
 	# install freeswitch fusionpbx install
-	for i in freeswitch freeswitch-init freeswitch-lang-en freeswitch-meta-codecs freeswitch-mod-avmd freeswitch-mod-callcenter \
-		freeswitch-mod-cidlookup freeswitch-mod-commands freeswitch-mod-conference freeswitch-mod-curl freeswitch-mod-db freeswitch-mod-dingaling \
-		freeswitch-mod-distributor freeswitch-mod-dptools freeswitch-mod-enum freeswitch-mod-esf freeswitch-mod-esl \
-		freeswitch-mod-expr freeswitch-mod-fifo freeswitch-mod-fsv freeswitch-mod-hash freeswitch-mod-memcache freeswitch-mod-portaudio \
-		freeswitch-mod-portaudio-stream freeswitch-mod-random freeswitch-mod-redis freeswitch-mod-sms freeswitch-mod-spandsp freeswitch-mod-spy \
-		freeswitch-mod-translate freeswitch-mod-valet-parking freeswitch-mod-vmd freeswitch-mod-flite freeswitch-mod-pocketsphinx freeswitch-mod-tts-commandline \
-		freeswitch-mod-dialplan-xml freeswitch-mod-loopback freeswitch-mod-rtmp freeswitch-mod-sofia freeswitch-mod-event-multicast freeswitch-mod-event-socket \
-		freeswitch-mod-event-test freeswitch-mod-local-stream freeswitch-mod-native-file freeswitch-mod-sndfile freeswitch-mod-tone-stream freeswitch-mod-lua \
-		freeswitch-mod-console freeswitch-mod-logfile freeswitch-mod-syslog freeswitch-mod-say-en freeswitch-mod-posix-timer freeswitch-mod-timerfd \
-		freeswitch-mod-xml-cdr freeswitch-mod-xml-curl freeswitch-mod-xml-rpc ;do apt-get -y install --force-yes "${i}" ;done
+	for i in freeswitch freeswitch-init freeswitch-meta-codecs freeswitch-mod-avmd freeswitch-mod-callcenter freeswitch-mod-cidlookup \
+		freeswitch-mod-commands freeswitch-mod-conference freeswitch-mod-curl freeswitch-mod-db freeswitch-mod-dingaling freeswitch-mod-distributor \
+		freeswitch-mod-dptools freeswitch-mod-enum freeswitch-mod-esf freeswitch-mod-esl freeswitch-mod-expr freeswitch-mod-fifo freeswitch-mod-fsv \
+		freeswitch-mod-hash freeswitch-mod-memcache freeswitch-mod-portaudio freeswitch-mod-portaudio-stream freeswitch-mod-random freeswitch-mod-redis \
+		freeswitch-mod-sms freeswitch-mod-spandsp freeswitch-mod-spy freeswitch-mod-translate freeswitch-mod-valet-parking freeswitch-mod-vmd freeswitch-mod-flite \
+		freeswitch-mod-pocketsphinx freeswitch-mod-tts-commandline freeswitch-mod-dialplan-xml freeswitch-mod-loopback freeswitch-mod-rtmp freeswitch-mod-sofia \
+		freeswitch-mod-event-multicast freeswitch-mod-event-socket freeswitch-mod-event-test freeswitch-mod-local-stream freeswitch-mod-native-file \
+		freeswitch-mod-sndfile freeswitch-mod-tone-stream freeswitch-mod-lua freeswitch-mod-console freeswitch-mod-logfile freeswitch-mod-syslog \
+		freeswitch-mod-say-en freeswitch-mod-posix-timer freeswitch-mod-timerfd freeswitch-mod-xml-cdr freeswitch-mod-xml-curl \
+		freeswitch-mod-xml-rpc freeswitch-lang-en ;do apt-get -y install --force-yes "${i}" ;done
 fi
 
 if [[ $freeswitch_install == "meta-lang" ]]; then
