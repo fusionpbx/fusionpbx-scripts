3eb953af25c10e9309789c5936f3af572de13420
Richard Neese <r.neese@gmail.com>
2014-02-13 20:57:35 +0000

    repost

--- install/Debian/Upgrade-Old-Fusion-Installs.sh
+++ install/Debian/Upgrade-Old-Fusion-Installs.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+#
 #Fusionpbx repo (stable/devel)
 fusionpbx_repo="devel"
-#
-echo ' THis script wiill upgrade you to the new fusionpbx pkgs'
-echo
-echo 'Removing Old FusionPBX Pkg'
+
+echo ' This script will upgrade you to the new fusionpbx pkgs '
+echo ' Removing Old FusionPBX Pkg '
 apt-get remove fusionpbx fusionpbx-dev
-#
+
+#updating repo 
+echo 'installing updated repo info into apt'
 if [[ $fusionpbx_repo == "stable" ]]; then
 echo 'installing fusionpbx stable repo'
 /bin/cat > "/etc/apt/sources.list.d/fusionpbx.list" <<DELIM
@@ -18,11 +20,12 @@ echo 'installing fusionpbx devel repo'
 deb http://repo.fusionpbx.com/deb-dev/debian/ wheezy main
 DELIM
 fi
-#
+
 apt-get update
-#
+
 echo ' Installing New Fusionpbx pkgs'
 echo " Installing fusipnpbx full install"
+
 for i in fusionpbx-core fusionpbx-theme-accessible fusionpbx-theme-classic fusionpbx-theme-default fusionpbx-theme-enhanced fusionpbx-theme-nature \
 		fusionpbx-app-adminer fusionpbx-app-call-block fusionpbx-app-call-broadcast fusionpbx-app-call-center fusionpbx-app-call-center-active \
 		fusionpbx-app-call-flows fusionpbx-app-calls fusionpbx-app-calls-active fusionpbx-app-click-to-call fusionpbx-app-conference-centers fusionpbx-app-conferences \
@@ -33,11 +36,12 @@ for i in fusionpbx-core fusionpbx-theme-accessible fusionpbx-theme-classic fusio
 		fusionpbx-app-provision fusionpbx-app-recordings fusionpbx-app-registrations fusionpbx-app-ring-groups fusionpbx-app-schemas fusionpbx-app-services \
 		fusionpbx-app-settings fusionpbx-app-sip-profiles fusionpbx-app-sip-status fusionpbx-app-sql-query fusionpbx-app-system fusionpbx-app-time-conditions \
 		fusionpbx-app-traffic-graph fusionpbx-app-vars fusionpbx-app-voicemail-greetings fusionpbx-app-voicemails fusionpbx-app-xml-cdr fusionpbx-app-xmpp
+
 do apt-get -y --force-yes install "${i}"
 done
-#
+
 cd /usr/share/nginx/www/fusionpbx
-#
+
 php /usr/share/nginx/www/fusionpbx/core/upgrade/upgrade.php
-#
+
 echo ' upgrade to new fusion pkgs is complete '
