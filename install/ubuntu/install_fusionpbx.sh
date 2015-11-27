af3985fd86d2678a580e376cf2833f84a104a2f9
Matthew Vale <github@mafoo.org>
2015-11-27 17:09:40 +0000

    replaced text removed when merging

--- install/ubuntu/install_fusionpbx.sh
+++ install/ubuntu/install_fusionpbx.sh
@@ -910,9 +910,12 @@ CURMD5=$(md5sum "$WHEREAMI" | sed -e "s/\ .*//")
 echo "The md5sum of the current script is: $CURMD5"
 NEWMD5=$(md5sum /tmp/install_fusionpbx.latest | sed -e "s/\ .*//")
 echo "The md5sum of the latest script is: $NEWMD5"
+
 if [[ "$CURMD5" == "$NEWMD5" ]]; then
 	echo "files are the same, continuing"
 else
+	echo "There is a new version of this script."
+	echo "  It is PROBABLY a good idea use the new version"
 	echo "  the new file is saved in /tmp/install_fusionpbx.latest"
 	echo "  to see the difference, run:"
 	echo "  diff -y /tmp/install_fusionpbx.latest $WHEREAMI"
