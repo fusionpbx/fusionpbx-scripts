5c234dbef616bf28b45d4c9ca25b5c8e871c8dcd
markjcrane <markjcrane@gmail.com>
2016-01-28 10:35:16 -0700

    Disable make -j for now as it might be causing the compile to fail.

--- install/debian/install_fusionpbx.sh
+++ install/debian/install_fusionpbx.sh
@@ -82,8 +82,8 @@ FSStablefile=freeswitch-1.6.6
 FSDB=p
 
 #right now, make -j not working. see: jira FS-3005
-CORES=$(/bin/grep processor -c /proc/cpuinfo)
-#CORES=1
+#CORES=$(/bin/grep processor -c /proc/cpuinfo)
+CORES=1
 FQDN=$(hostname -f)
 #SRCPATH="/usr/src/freeswitch" #DEFAULT
 SRCPATH="/usr/src/freeswitch"
