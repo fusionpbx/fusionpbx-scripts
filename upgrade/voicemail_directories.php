1bc66e6038c6e5c2d807ba60166cebbef5ca40f6
markjcrane <markjcrane@gmail.com>
2016-07-01 12:11:20 -0600

    Update voicemail directories.php

--- upgrade/voicemail_directories.php
+++ upgrade/voicemail_directories.php
@@ -1,18 +1,24 @@
 <?php
 
+//migrate existing attachment preferences to new column, where appropriate
+	$sql = "update v_voicemails set voicemail_file = 'attach' where voicemail_attach_file = 'true'";
+	//$db->exec(check_sql($sql));
+	//unset($sql);
 
-$storage_dir = $_SESSION['switch']['storage']['dir'];
-$sql = "select * from v_voicemails ";
-$prep_statement = $db->prepare(check_sql($sql));
-$prep_statement->execute();
-$result = $prep_statement->fetchAll(PDO::FETCH_NAMED);
-foreach ($result as &$row) {
-	$domain_name = $_SESSION['domains'][$row["domain_uuid"]]['domain_name'];
-	$voicemail_dir = $storage_dir . '/voicemail/default/'.$domain_name;
-	if (!mkdir($voicemail_dir, 0744, true)) {
-		die('Failed to create '.$voicemail_dir.');
+//add that the directory structure for voicemail each domain and voicemail id is
+	$sql = "select d.domain_name, v.voicemail_id ";
+	$sql .= "from v_domains as d, v_voicemails as v ";
+	$sql .= "where v.domain_uuid = d.domain_uuid ";
+	$prep_statement = $db->prepare($sql);
+	$prep_statement->execute();
+	$voicemails = $prep_statement->fetchAll(PDO::FETCH_ASSOC);
+	foreach ($voicemails as $row) {
+		$path = $_SESSION['switch']['voicemail']['dir'].'/default/'.$row['domain_name'].'/'.$row['voicemail_id'];
+		if (!file_exists($path)) {
+			event_socket_mkdir($path);
+			//mkdir($path, 0744, true);
+		}
 	}
-}
-unset ($prep_statement);
+	unset ($prep_statement, $sql);
 
 ?>
